99927166783241788728532729cff48cd507c427
vasilty <vasilty@gmail.com>
2016-10-02 15:38:26 +0200

    Added postgres db settings

--- team-builder/team_builder/deploy_settings/__init__.py
+++ team-builder/team_builder/deploy_settings/__init__.py
@@ -1,3 +1,5 @@
+import dj_database_url
+
 from team_builder.settings import *
 
 DEBUG = False
@@ -9,3 +11,6 @@ ALLOWED_HOSTS = [
 ]
 
 SECRET_KEY = get_env_variable('SECRET_KEY')
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
