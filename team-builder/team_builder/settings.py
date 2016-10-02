6ae03bd675d71798dab00dd02c3c959020c8c4b3
vasilty <vasilty@gmail.com>
2016-10-02 15:34:44 +0200

    Added env var getter

--- team-builder/team_builder/settings.py
+++ team-builder/team_builder/settings.py
@@ -13,6 +13,17 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the {} env variable".format(var_name)
+        if DEBUG:
+            warnings.warn(error_msg)
+        else:
+            raise ImproperlyConfigured(error_msg)
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
